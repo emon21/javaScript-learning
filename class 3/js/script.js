405a9e68c6b098ffc130149fff85a4430c3ec2f6
emon21 <emonbd66@gmail.com>
2022-12-19 16:45:30 +0600

    prject setup

---
+++ class 3/js/script.js
@@ -0,0 +1,63 @@
+// class 1 js code write
+
+// console.log(10);
+// console.log('Hasibur Rahan Emon'+' '+'Fronten Developer');
+
+ /* let title = document.write("Emon Ahmed Raj"+ '->' + "Ear");
+    document.getElementById($title); */
+
+// class 2 variable
+
+//Function
+
+// function Test(){
+
+// document.write('Hasib Raj');
+
+// }
+
+// function introductin(){
+//     document.write('hasib');
+//     document.write('<hr/>');
+//     document.write('25');
+//     document.write('<br>');
+//     document.write('01811287256');
+// }
+
+// introductin();
+
+//variable
+
+// var x = 'hasib';
+
+// var gloibalvar = 'its global var';
+
+// function fun(){
+//     var localvar ='localvariable';
+//     console.log(gloibalvar);
+//     console.log(localvar);
+// }
+
+//  //console.log(x);
+
+//  fun();
+
+//  function run(){
+//     var localvar ='localvariable';
+
+//     console.log(gloibalvar);
+//     console.log(localvar);
+//  }
+
+//  run();
+
+
+
+//  class 3  Data type
+
+
+
+
+
+
+
---
+++ js/script.js
@@ -0,0 +1,33 @@
+
+//  class 3  Data type
+
+// var x =10;
+// var y ='Hasib';
+// var z = x + y;
+// console.log(typeof x);
+// console.log(typeof y);
+
+
+
+//array
+
+// var age = [18,35,78];
+// var age = ['limon','hasib','arif'];
+// var person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+// // let x = BigInt("123456789012345678901234567890");
+// console.log(x);
+
+
+//variable of keywords
+
+const hasib = 'Dhaka'; //constant value do not change
+console.log(hasib);
+
+let country = 'India'; //let value change
+country = 'Bangladesh';
+console.log(country);
+
+
+var x1 =true;
+var x2 = false;
+
