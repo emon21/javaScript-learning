70ea70644a5546b1741bb24c3297f74d5c406be7
emon21 <emonbd66@gmail.com>
2022-12-20 17:21:57 +0600

    feat:added class 4 array

---
+++ class 4/array.js
@@ -0,0 +1,120 @@
+
+
+// const hello = ['hi','demp','khaili'];
+
+// const multiData = [
+// 100,true,'hasib',{}
+// ]
+
+
+//array const 
+
+// const names = new Array('ARIF','zafor','hasib');
+
+// const cartPage = [ ];
+
+// const number = [25,45,62,78,56,30];
+
+
+//array er moddy muiltiple data pase kora holo with obj/function/array
+// constnewData = [
+//     {task:expernece},
+//     [1,2,3,4,5],
+//     function hello(){
+//         console.log('hi');
+//     }
+
+// ]
+
+//array 
+
+const letters = ['a','b','c','d','e'];
+
+// console.log(letters[0]);
+// console.log(letters[1]);
+// console.log(letters[2]);
+// console.log(letters[3]);
+// console.log(letters[4]);
+
+
+//array er moddy element added 2 types : 
+
+//first data added
+
+//array fast data add kora 
+//letters.unshift('hasib'); //-> method added unshift
+
+//last data added
+
+//array last data add kora
+letters.push('f');  //-> method added push
+
+
+
+
+//array data change kora jonno  
+//letters.pop('hasib'); //-> method added unshift keywords
+letters[6] = 'raj' //->  added keywords
+
+//array last element  data remove kora jonno  
+
+ letters.pop();  //-> method added unshift
+
+const remove = letters.pop();  //-> remove kora element dakher jonno
+
+// Remove kora jonno element
+
+// first data remove of array
+
+letters.shift();
+
+// Add korar jonno Element
+
+
+// Update/change korar jonno Element
+
+//array length/sonkha bar kora
+
+//letters.length();
+
+// console.log(letters.length); //array method
+
+
+
+//array method sort
+//data serial moto  show hoi
+
+// const demo = ['mango','Banana','Apple','Fig','Orange','Watermelon','Lemon','Tomato','Cherry'];
+
+// const rr = demo.find(Element => Element > 4);
+
+// console.log(rr);
+
+//console.log(demo.sort());
+
+
+//console.log(typeof names);
+
+
+//array of splish method
+// const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"]; //array value
+
+// const citrus = fruits.slice(1,3); //start ,number +
+// const citrus = fruits.slice(-3,-1); //start ,number revers -
+
+// const dd = fruits.splice(3, 3); //index number and count or koita show hoby
+
+// console.log(dd);
+
+
+//array key method
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// const keys = Object.keys(fruits);
+
+// console.log(keys);
+
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+const demo = fruits.length = 1;
+console.log(demo);
